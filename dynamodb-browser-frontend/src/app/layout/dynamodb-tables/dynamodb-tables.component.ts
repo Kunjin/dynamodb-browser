60470b02200b5c9dd158620c3e0f1116cf461980
Cyra Abigail Betco <cyra.betco@gmail.com>
2019-03-29 21:50:10 +0800

    [frontend]-reinitialized data table upon selecting new table

--- dynamodb-browser-frontend/src/app/layout/dynamodb-tables/dynamodb-tables.component.ts
+++ dynamodb-browser-frontend/src/app/layout/dynamodb-tables/dynamodb-tables.component.ts
@@ -15,33 +15,36 @@ export class DynamodbTablesComponent implements OnInit {
 
     columnDefs = [];
     dataSource: MatTableDataSource<any>;
-    data='AAA';
 
     constructor(private transactionsService: TransactionService,
-                private activatedRoute: ActivatedRoute,) {
+                private activatedRoute: ActivatedRoute) {
     }
 
     ngOnInit(): void {
         this.activatedRoute.params.subscribe(params => {
+            this.reinitializeDataTable();
             const tableParam = params['table'];
             this.transactionsService.getRecordsByTables(tableParam).subscribe(records => {
                 for (let column in JSON.parse(records[0])) {
                     this.columnDefs.push(column);
                 }
                 this.dataSource = records;
-                console.log(records);
                 let recordsArr = [];
                 //TODO: For now.. Look on how to handle this better!!
                 for (let i=0; i < records.length; i++) {
                     recordsArr.push(JSON.parse(records[i]));
                 }
-                this.initializeDataTable(recordsArr);
+                this.addDataInDataTable(recordsArr);
             })
         });
     }
 
-    private initializeDataTable(arr: any[]) {
+    private reinitializeDataTable() {
+        this.dataSource = new MatTableDataSource<any>();
+        this.columnDefs = [];
+    }
+
+    private addDataInDataTable(arr: any[]) {
         this.dataSource = new MatTableDataSource(arr);
-        console.log(this.dataSource);
     }
 }
